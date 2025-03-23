f72c83368613ba733dbf56f4781064ed9ed6a99d
Tommy <Tommy@DESKTOP-O42VPU1.fritz.box>
2025-03-23 18:39:09 +0100

    client funzionante

--- webService_Bassoni_Client/src/main/java/it/edu/marconipontedera/tepsit/client/Main.java
+++ webService_Bassoni_Client/src/main/java/it/edu/marconipontedera/tepsit/client/Main.java
@@ -12,9 +12,13 @@ import junit.textui.TestRunner;
 
 public class Main {
     public static void main(String[] args) {
-    	
+    	testGet();
+    	testAdd();
+    }
+    
+    public static void testGet() {
     	Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/api").path("books").path("1");
+    	WebTarget target = client.target("http://localhost:8080/api").path("books").path("1");
 		Response response = target.request().get();
 		System.out.println("Response code: " + response.getStatus());
 		Book book = response.readEntity(Book.class);
